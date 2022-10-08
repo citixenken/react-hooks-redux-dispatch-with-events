c1ad3633a9909b10f879a420fd7ae9c655740774
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-10-08 11:25:36 +0300

    COMPLETED: Completing our Counter Application

--- src/reducer.js
+++ src/reducer.js
@@ -1,1 +1,30 @@
-// add code snippets from README
+let state;
+
+function reducer(state = { count: 0 }, action) {
+  switch (action.type) {
+    case "counter/increment":
+      return { count: state.count + 1 };
+    default:
+      return state;
+  }
+}
+
+function render() {
+  let container = document.getElementById("container");
+  container.textContent = state.count;
+}
+
+function dispatch(action) {
+  state = reducer(state, action);
+  render();
+}
+
+dispatch({ type: "@@INIT" });
+
+// integrating dispatch with user event
+let button = document.getElementById("button");
+let action = { type: "counter/increment" };
+
+button.addEventListener("click", () => {
+  dispatch(action);
+});
